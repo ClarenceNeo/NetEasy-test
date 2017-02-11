f47f162c54bc5636d18cdb853977c36da1bfe99c
ClarenceNeo <niuzhiyuan94@gmail.com>
2017-02-11 15:08:04 +0800

    git add .

--- JS_DOM/scripts/showPic.js
+++ JS_DOM/scripts/showPic.js
@@ -1,33 +1,52 @@
-function showPic(whichpic) {
-  if (!document.getElementById("placeholder")) return false;
-  var source = whichpic.getAttribute("href");
-  var placeholder = document.getElementById("placeholder");
-  if (placeholder.nodeName != "IMG") return false;
-  placeholder.setAttribute("src",source);
-  // placeholder.src = source; // 另一种写法，可移植性好
-  if (document.getElementById("description")) {
-    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
-    var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
   }
-  return true;
 }
-// window.onload = prepareLinks;
-// function prepareLinks() {
-//   if (!document.getElementsByTagName) return false;
-//   var links = document.getElementsByTagName("a");
-//   for (var i=0; i<links.length; i++) {
-//     if (links[i].getAttribute("class") == "popup") {
-//       links[i].onclick = function() {
-//         popUp(this.getAttribute("href"));
-//         return false;
-//       }
-//     }
-//   }
-// }
-// function popUp(winURL) {
-//   window.open(winURL,"popup","width=320,height=480")
-// }
+
+// 编写 insertAfter 函数
+function insertAfter(newElement,targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
+}
+
+
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var placeholder = document.createElement("img");
+  placeholder.setAttribute("id", "placeholder");
+  placeholder.setAttribute("src", "images/placeholder.png");
+  placeholder.setAttribute("alt", "my image gallery");
+  var description = document.createElement("p");
+  description.setAttribute("id", "description");
+  var desctext = document.createTextNode("Choose an image.");
+  description.appendChild(desctext);
+
+  // document.getElementsByTagName("body")[0].appendChild(placeholder);
+  // document.getElementsByTagName("body")[0].appendChild(description);
+
+  // var gallery = document.getElementById("imagegallery");
+  // gallery.parentNode.insertBefore(placeholder,gallery);
+  // gallery.parentNode.insertBefore(description,gallery);
+
+  var gallery = document.getElementById("imagegallery");
+  insertAfter(placeholder,gallery);
+  insertAfter(description,placeholder);
+}
+
 
 function prepareGallery() {
   if (!document.getElementsByTagName || !document.getElementById) return false;
@@ -42,20 +61,21 @@ function prepareGallery() {
   }
 }
 
-// window.onload = function() {
-//   prepareGallery();
-// }
 
-function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function() {
-      oldonload();
-      func();
-    }
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return false;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  if (placeholder.nodeName != "IMG") return false;
+  placeholder.setAttribute("src",source);
+  // placeholder.src = source; // 另一种写法，可移植性好
+  if (document.getElementById("description")) {
+    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
   }
+  return true;
 }
 
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
